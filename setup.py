d24ace612540e8f18867681b3d170fa907f97586
valentin-burillier <93446869+valentin-burillier@users.noreply.github.com>
2022-06-15 18:47:33 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'spiderpen',
+    packages = ['spiderpen'],
+    version = '0.1.1',
+    description = 'Python library to control a complementary model of the LEGO Boost 17101 set',
+    author = 'Valentin Burillier',
+    author_email = '',
+    url = 'https://github.com/valentin-burillier/spiderpen',
+    keywords = ['LEGO', 'ROBOTICS'],
+    requires=['pylgbst'],
+)

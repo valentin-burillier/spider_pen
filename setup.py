80aef0ac4bde57a26aafdb240ea37d4a658058c8
valentin-burillier <93446869+valentin-burillier@users.noreply.github.com>
2022-06-25 20:04:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'spiderpen',
     packages = ['spiderpen'],
-    version = '0.0.10',
+    version = '0.0.12',
     description = 'Python library for controlling a plotter model complementary to the LEGO Boost 17101 set',
     long_description = '',
     author = 'Valentin Burillier',

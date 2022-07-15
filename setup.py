c6e7449d461b92368f27d10cde5621e9f0509f41
valentin-burillier <93446869+valentin-burillier@users.noreply.github.com>
2022-07-15 12:02:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 setup(
     name = 'spiderpen',
     packages = ['spiderpen'],
-    version = '0.0.12',
+    version = '0.1.0',
     description = 'Python library for controlling a plotter model complementary to the LEGO Boost 17101 set',
     long_description = '',
     author = 'Valentin Burillier',
-    author_email = '',
+    author_email = 'burillier.v@gmail.com',
     url = 'https://github.com/valentin-burillier/spiderpen',
     keywords = ['LEGO', 'ROBOTICS'],
     requires=['pylgbst'],
